d7b34bb480fec55d214c487a8f996b3e550de40d
nick <nicholasjpenaranda@gmail.com>
2022-08-15 17:51:09 -0500

    Add airbnb style rules

--- src/dommanip.js
+++ src/dommanip.js
@@ -1,108 +1,109 @@
-import {projectArr, currentProject, updateStorage,} from "./index.js"
+/* eslint-disable no-alert */
+import { projectArr, currentProject, updateStorage } from './index.js';
 
-const projectContainer = document.querySelector("#projectContainer")
-const todoContainer = document.querySelector("#todoContainer")
-const submitProject = document.querySelector("#submitProject")
-const submitTodo = document.querySelector("#submitTodo")
-let titleInput = false
-let dateInput = false
+const projectContainer = document.querySelector('#projectContainer');
+const todoContainer = document.querySelector('#todoContainer');
+const submitProject = document.querySelector('#submitProject');
+const submitTodo = document.querySelector('#submitTodo');
+let titleInput = false;
+let dateInput = false;
 
 function domManip() {
-    projectContainer.innerHTML = ""
-    todoContainer.innerHTML= ""
-    projectArr.forEach(project => {
-        projectToHtml(project);
-    });
-    currentProject.list.forEach(task => {
-        taskToHtml(task)
-    })
-}; 
+  projectContainer.innerHTML = '';
+  todoContainer.innerHTML = '';
+  projectArr.forEach((project) => {
+    projectToHtml(project);
+  });
+  currentProject.list.forEach((task) => {
+    taskToHtml(task);
+  });
+}
 
 function projectToHtml(project) {
-    const _projectContainer = document.querySelector("#projectContainer")
-    const _div = document.createElement("div")
-    const _button = document.createElement("button")
-    const _button2 = document.createElement("button")
-    _button.textContent = `${project.name}`
-    _div.appendChild(_button)
-    _button2.textContent = "X"
-    _div.appendChild(_button2)
-    _projectContainer.append(_div)
+  const _projectContainer = document.querySelector('#projectContainer');
+  const _div = document.createElement('div');
+  const _button = document.createElement('button');
+  const _button2 = document.createElement('button');
+  _button.textContent = `${project.name}`;
+  _div.appendChild(_button);
+  _button2.textContent = 'X';
+  _div.appendChild(_button2);
+  _projectContainer.append(_div);
 
-    document.querySelector("#title").textContent = currentProject.name
+  document.querySelector('#title').textContent = currentProject.name;
 
-    //project switch 
-    _button.addEventListener("click", () => {
-        currentProject = projectArr[projectArr.indexOf(project)]
-        domManip()
-        updateStorage()
-    })
+  // project switch
+  _button.addEventListener('click', () => {
+    currentProject = projectArr[projectArr.indexOf(project)];
+    domManip();
+    updateStorage();
+  });
 
-    //delete button
-    _button2.addEventListener("click", () => {
-        projectArr.splice(projectArr.indexOf(project), 1)
-        domManip()
-        updateStorage()
-    })
+  // delete button
+  _button2.addEventListener('click', () => {
+    projectArr.splice(projectArr.indexOf(project), 1);
+    domManip();
+    updateStorage();
+  });
 }
 
 function taskToHtml(task) {
-    const _todoContainer = document.querySelector("#todoContainer")
-    const _div = document.createElement("div")
-    const _divInDiv = document.createElement("div")
-    const _span = document.createElement("span")
-    const _span2 = document.createElement("span")
-    const _button2 = document.createElement("button")
+  const _todoContainer = document.querySelector('#todoContainer');
+  const _div = document.createElement('div');
+  const _divInDiv = document.createElement('div');
+  const _span = document.createElement('span');
+  const _span2 = document.createElement('span');
+  const _button2 = document.createElement('button');
 
-    _span.textContent = `${task.title}`
-    _divInDiv.appendChild(_span)
-    _span2.textContent = task["due date"] === "" ? "No Due Date Set" : `${task["due date"]}`
-    _divInDiv.appendChild(_span2)
-    _div.appendChild(_divInDiv)
-    _button2.textContent = "X"
-    _div.appendChild(_button2)
-    _todoContainer.appendChild(_div)
+  _span.textContent = `${task.title}`;
+  _divInDiv.appendChild(_span);
+  _span2.textContent = task['due date'] === '' ? 'No Due Date Set' : `${task['due date']}`;
+  _divInDiv.appendChild(_span2);
+  _div.appendChild(_divInDiv);
+  _button2.textContent = 'X';
+  _div.appendChild(_button2);
+  _todoContainer.appendChild(_div);
 
-    _span.addEventListener("click", () => {
-        if(titleInput === true) {return}
-        titleInput = true
-        _span.innerHTML = '<input type="text" value="' + _span.textContent + '" onblur="this.focus()" autofocus>'
-        _span.firstChild.addEventListener("keypress", (e) => {
-            if (e.key === "Enter") {
-                if(_span.firstChild.value === "") {return alert("Needs at least one character.")}
-                task.title = _span.firstChild.value
-                domManip()
-                updateStorage()
-                titleInput = false
-            }
-        })
-    })
+  _span.addEventListener('click', () => {
+    if (titleInput === true) { return; }
+    titleInput = true;
+    _span.innerHTML = `<input type="text" value="${_span.textContent}" onblur="this.focus()" autofocus>`;
+    _span.firstChild.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        if (_span.firstChild.value === '') { return alert('Needs at least one character.'); }
+        task.title = _span.firstChild.value;
+        domManip();
+        updateStorage();
+        titleInput = false;
+      }
+    });
+  });
 
-    _span2.addEventListener("click", () => {
-        if(dateInput === true) {return}
-        dateInput = true
-        _span2.innerHTML = '<input type="date" value="' + _span.textContent + '" onblur="this.focus()" autofocus>'
-        _span2.firstChild.addEventListener("keypress", (e) => {
-            if (e.key === "Enter") {
-                task["due date"] = _span2.firstChild.value
-                domManip()
-                updateStorage()
-                dateInput = false
-            }
-        })
-    })
+  _span2.addEventListener('click', () => {
+    if (dateInput === true) { return; }
+    dateInput = true;
+    _span2.innerHTML = `<input type="date" value="${_span.textContent}" onblur="this.focus()" autofocus>`;
+    _span2.firstChild.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        task['due date'] = _span2.firstChild.value;
+        domManip();
+        updateStorage();
+        dateInput = false;
+      }
+    });
+  });
 
-    _button2.addEventListener("click", () => {
-        currentProject.list.splice(currentProject.list.indexOf(task), 1)
-        domManip()
-        updateStorage()
-    })
+  _button2.addEventListener('click', () => {
+    currentProject.list.splice(currentProject.list.indexOf(task), 1);
+    domManip();
+    updateStorage();
+  });
 }
 
 export {
-    domManip,
-    projectContainer,
-    todoContainer,
-    submitProject,
-    submitTodo,
-}
+  domManip,
+  projectContainer,
+  todoContainer,
+  submitProject,
+  submitTodo,
+};
--- src/index.js
+++ src/index.js
@@ -1,68 +1,85 @@
-import {domManip, projectContainer, todoContainer, submitProject, submitTodo,} from "./dommanip.js"
-//import {storageAvailable, updateStorage} from "./storage.js"
+/* eslint-disable no-undef */
+import {
+  domManip,
+  projectContainer,
+  submitProject,
+  submitTodo,
+} from "./dommanip.js";
 
-let currentProject = {}
-let projectArr = []
+let currentProject = {};
+let projectArr = [];
 
 if (localStorage.getItem("allProjects") === null) {
-    currentProject = {name: "default", list: [{title: "This is a todo example.", "due date": "10/10/2022"}],}
-    projectArr = [{name: "default", list: [{title: "This is a todo example.", "due date": "10/10/2022"}],}]
+  currentProject = {
+    name: "default",
+    list: [{ title: "This is a todo example.", "due date": "10/10/2022" }],
+  };
+  projectArr = [
+    {
+      name: "default",
+      list: [{ title: "This is a todo example.", "due date": "10/10/2022" }],
+    },
+  ];
 } else {
-    currentProject = JSON.parse(localStorage.getItem('selectedProject'))
-    projectArr = JSON.parse(localStorage.getItem("allProjects"))
+  currentProject = JSON.parse(localStorage.getItem("selectedProject"));
+  projectArr = JSON.parse(localStorage.getItem("allProjects"));
 }
 
 function updateStorage() {
-    localStorage.setItem('selectedProject', JSON.stringify(currentProject))
-    localStorage.setItem("allProjects", JSON.stringify(projectArr))
+  localStorage.setItem("selectedProject", JSON.stringify(currentProject));
+  localStorage.setItem("allProjects", JSON.stringify(projectArr));
 }
 
 function projects(name) {
-    return {
-        "name": name,
-         list: [],
-    }
+  return {
+    name,
+    list: [],
+  };
 }
 
 function todos(title, dueDate) {
-    return {
-        "title": title,
-        "due date": dueDate,
-    }
+  return {
+    title,
+    "due date": dueDate,
+  };
 }
 
 function createProject() {
-    const _newProject = document.querySelector("#newProject").value
-    if (_newProject === "") {return}
-    projectArr.push(projects(_newProject))
-    domManip()
-    updateStorage()
-    document.querySelector("#newProject").value = ""
-    projectContainer.lastChild.firstChild.click()
+  const _newProject = document.querySelector("#newProject").value;
+  if (_newProject === "") {
+    return;
+  }
+  projectArr.push(projects(_newProject));
+  domManip();
+  updateStorage();
+  document.querySelector("#newProject").value = "";
+  projectContainer.lastChild.firstChild.click();
 }
 
 function createTodo(project) {
-    const _newTodo = document.querySelector("#newTodo").value
-    if (_newTodo === "") {return}
-    const _newTodoDate = document.querySelector("#newTodoDate").value
-    projectArr[projectArr.indexOf(project)].list.push(todos(_newTodo, _newTodoDate))
-    domManip()
-    updateStorage()
-    document.querySelector("#newTodo").value = ""
+  const _newTodo = document.querySelector("#newTodo").value;
+  if (_newTodo === "") {
+    return;
+  }
+  const _newTodoDate = document.querySelector("#newTodoDate").value;
+  projectArr[projectArr.indexOf(project)].list.push(
+    todos(_newTodo, _newTodoDate)
+  );
+  domManip();
+  updateStorage();
+  document.querySelector("#newTodo").value = "";
 }
 
-submitProject.addEventListener("click", createProject)
+submitProject.addEventListener("click", createProject);
 
 submitTodo.addEventListener("click", () => {
-    if (projectArr[0] === undefined) {return}
-    createTodo(currentProject)
-})
+  if (projectArr[0] === undefined) {
+    return;
+  }
+  createTodo(currentProject);
+});
 
-domManip()
-projectContainer.lastChild.firstChild.click()
+domManip();
+projectContainer.lastChild.firstChild.click();
 
-export {
-    projectArr,
-    currentProject,
-    updateStorage,
-}
+export { projectArr, currentProject, updateStorage };
